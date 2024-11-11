d9a7b093921a0bd4fbff3bb78628b03f80759bce
Pritam Dhara <46365255+pritam1813@users.noreply.github.com>
2024-11-11 19:24:34 +0530

    fix callback route for vercel build

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,22 +1,27 @@
 import { NextResponse } from "next/server";
-import { type NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 import { createClient } from "@/app/utils/supabase/server";
 
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
+  const next = requestUrl.searchParams.get("next") ?? "/";
+  const redirectTo = request.nextUrl.clone();
+  redirectTo.pathname = next;
 
   if (code) {
     const supabase = await createClient();
 
-    const { data, error } = await supabase.auth.exchangeCodeForSession(code);
-    console.log("callback: ", data);
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    // console.log("callback: ", data);
 
     if (!error) {
-      return NextResponse.redirect(new URL("/dashboard", request.url));
+      redirectTo.pathname = "/dashboard";
+      return NextResponse.redirect(redirectTo);
     }
   }
 
   // Return the user to an error page with some instructions
-  return NextResponse.redirect(new URL("/error", request.url));
+  redirectTo.pathname = "/error";
+  return NextResponse.redirect(redirectTo);
 }
--- app/api/auth/confirm/route.ts
+++ app/api/auth/confirm/route.ts
@@ -14,7 +14,7 @@ export async function GET(request: NextRequest) {
   const redirectTo = request.nextUrl.clone();
   redirectTo.pathname = next;
 
-  console.log("REdirext: ", redirectTo);
+  // console.log("REdirext: ", redirectTo);
 
   const supabase = await createClient();
 

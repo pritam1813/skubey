474c89d081d0bf9de95f1ff1f1c15d2e35ff5031
Pritam Dhara <46365255+pritam1813@users.noreply.github.com>
2024-12-20 20:59:00 +0530

    sample testing automated tests

--- components/Buttons/ThemeLinkButton.tsx
+++ components/Buttons/ThemeLinkButton.tsx
@@ -5,7 +5,7 @@ const ThemeButton = ({ title, href }: { title: string; href: string }) => {
   return (
     <Link
       href={href}
-      className="tw-text-sm lg:tw-text-base tw-bg-primary tw-text-secondary hover:tw-bg-primaryHover hover:tw-text-primary tw-no-underline tw-py-2.5 tw-px-5 tw-rounded-pillcustom tw-transition-all tw-duration-500 tw-ease-in-out"
+      className="lg:tw-text-base tw-bg-primary tw-text-secondary hover:tw-bg-primaryHover hover:tw-text-primary tw-no-underline tw-py-2.5 tw-px-5 tw-rounded-pillcustom tw-transition-all tw-duration-500 tw-ease-in-out"
     >
       {title}
     </Link>

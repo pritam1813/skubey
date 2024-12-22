38c3f661d7b78a75cc2ec4237cb2ed9a53d59333
Pritam Dhara <46365255+pritam1813@users.noreply.github.com>
2024-12-22 18:14:10 +0530

    passed tests

--- components/Buttons/ThemeLinkButton.tsx
+++ components/Buttons/ThemeLinkButton.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const ThemeButton = ({ title, href }: { title: string; href: string }) => {
   return (
     <Link
-      href={`l/${href}`}
+      href={href}
       className="tw-text-sm lg:tw-text-base tw-bg-primary tw-text-secondary hover:tw-bg-primaryHover hover:tw-text-primary tw-no-underline tw-py-2.5 tw-px-5 tw-rounded-pillcustom tw-transition-all tw-duration-500 tw-ease-in-out"
     >
       {title}
